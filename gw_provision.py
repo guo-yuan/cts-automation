2be7fe7302f3286568b124c6f4e54869753620c8
zhanggy <guoyuan.zhang@technicolor.com>
2019-10-21 15:34:50 +1100

    Basic test case enhancement with exception handling and json report generation.

--- gw_provision.py
+++ gw_provision.py
@@ -7,9 +7,12 @@ Usage:
     python(python3) gw_provision.py <test_case>
 """
 
-import sys, os, time
+import sys, os, time, json, datetime
+# import paramiko
 from pyHS100 import SmartPlug
 from test_config import *
+
+report = {'case': '', 'result': '', 'reason': '', 'timestamp': ''}
 
 
 def basic_test():
@@ -18,35 +21,66 @@ def basic_test():
     Args: none
     Returns: none
     """
-    print('Step 1: power off DUT')
+
+    global report
+    report['case'] = 'basic'
+
+    print('Step 1: toggle DUT power')
     p = SmartPlug(smart_plug_ip)
-    #p.state = 'OFF'
-    time.sleep(3)
+    try:
+        p.state = 'OFF'
+        time.sleep(3)
+        p.state = 'ON'
+    except Exception:
+        print('Fail to toggle DUT power')
+        report['result'] = 'Aborted'
+        report['reason'] = 'Fail to toggle DUT power'
+        return
 
-    print('Step 2: power on DUT')
-    #p.state = 'ON'
-    #time.sleep(120)
+    # time.sleep(120)
+    print('Wait and allow DUT to boot')
 
-    print('Step 3: GW controller action')
+    print('Step 2: GW controller action')
     shell_cmd = 'cd %s; %s %s H1.1 xDSL1 on' % (gw_controller_dir, gw_controller_exe, gw_controller_ip)
     print(shell_cmd)
-    #os.system(shell_cmd)
-    #time.sleep(120)
+    ret = os.system(shell_cmd)
+    if ret != 0:
+        print('Fail to run GW controller')
+        report['result'] = 'Aborted'
+        report['reason'] = 'Fail to run GW controller'
+        return
 
-    print('Step 4: Run speed test')
-    shell_cmd = 'cd %s; %s' % (speed_test_dir, speed_test_cmd)
-    print(shell_cmd)
-    #os.system(shell_cmd)
-    #time.sleep(60)
+    # time.sleep(120)
+    print('Wait and allow DUT WAN connection to connect')
 
-    print('Test completed')
+    print('Step 3: Check Internet connection on a CPE connected to DUT')
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(cpe_ip, username=cpe_ssh_username, password=cpe_ssh_passwd)
+    except Exception:
+        print('Fail to connect to test CPE')
+        report['result'] = 'Failed'
+        report['reason'] = 'Fail to connect test CPE'
+        return
+
+    shell_cmd = 'ping 8.8.8.8 -c 5'
+    _, ssh_stdout, _ = ssh.exec_command(shell_cmd)
+    if ssh_stdout.find('0% packet loss') == -1:
+        print('Fail to ping Internet from test CPE')
+        report['result'] = 'Failed'
+        report['reason'] = 'Fail to ping Internet from test CPE'
+        return
+
+    print('Test successfully completed')
+    report['result'] = 'Success'
 
 
 def run_test(case):
     """
     Main entry for GW provisioning tests.
 
-    :param test: test case selection
+    :param case: test case selection
     :return: none
     """
     if 'basic' == case:
@@ -63,3 +97,12 @@ if __name__ == '__main__':
         print('No test case specified.')
         exit(0)
     run_test(sys.argv[1])
+    test_timestamp = str(datetime.datetime.now())
+    report['timestamp'] = test_timestamp
+    json_report = json.dumps(report, indent=4, separators=(', ', ': '))
+    print(json_report)
+
+    fh = open('gw_provision_report.txt', 'w')
+    fh.write('GW provisioning test report generated at ' + test_timestamp + '\n\n')
+    fh.write(json_report)
+    fh.close()
